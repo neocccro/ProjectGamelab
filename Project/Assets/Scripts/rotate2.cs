3866d4592cb6fd9991b04a641d72a11bac52f5fe
Unknown <cavandiepen@gmail.com>
2018-05-23 12:22:44 +0200

    art

--- Project/Assets/Scripts/rotate2.cs
+++ Project/Assets/Scripts/rotate2.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rotate2 : MonoBehaviour {
+public class rotate2 : MonoBehaviour
+{
+    private float height;
+    private float width;
+    private float rotation;
+    private float speed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        speed = 0.2f;
+        height = Random.Range(2.0f, 10.0f);
+        width = height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation += speed / width;
+        gameObject.transform.position = new Vector3(Mathf.Sin(rotation) * width, height, Mathf.Cos(rotation) * width);
+    }
 }
