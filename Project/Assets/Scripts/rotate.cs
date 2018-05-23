d9574fe303f5e44ee5132094329bde1ea98df4d3
Unknown <cavandiepen@gmail.com>
2018-05-23 12:05:13 +0200

    rotation
    done?

---
+++ Project/Assets/Scripts/rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+    private float height;
+    private float width;
+    private float rotation;
+    private float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        height = Random.Range(0.0f, 10.0f);
+        width = Random.Range(0.0f, 10.0f);
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+        rotation += speed / width;
+        gameObject.transform.position = new Vector3(Mathf.Sin(rotation), Mathf.Cos(rotation), height);
+	}
+}
