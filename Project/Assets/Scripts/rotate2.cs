30d8f0b1ae53ce2e21cb963192782bf30b90d61e
Unknown <cavandiepen@gmail.com>
2018-05-23 12:18:52 +0200

    k
    k

--- Project/Assets/Scripts/rotate2.cs
+++ Project/Assets/Scripts/rotate2.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour {
+public class rotate2 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +11,6 @@ public class test : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		game
+		
 	}
 }
