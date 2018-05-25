4d8d283ff1a0da20f31db508b4814b4ffb2b25b1
nickvaningen <nickvaningen@hotmail.com>
2018-05-25 09:58:21 +0200

    raycast test V4

--- Project/Assets/Scripts/InputHandler.cs
+++ Project/Assets/Scripts/InputHandler.cs
@@ -3,19 +3,12 @@ using System.Collections;
 
 public class InputHandler : MonoBehaviour
 {
+    void start() { print("entro"); }
     void Update()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            
-            Plane plane = new Plane(Vector3.up, transform.position);
-            float distance = 0; 
-            if (plane.Raycast(ray, out distance))
-            { 
-                Vector3 pos = ray.GetPoint(distance); 
-            }
-        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, 100))
+            print("yolo");
     }
 }
