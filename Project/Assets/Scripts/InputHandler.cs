38ca3593c06d6aa0231ca713ad48f112505b531a
Unknown <cavandiepen@gmail.com>
2018-05-25 10:20:39 +0200

    k

--- Project/Assets/Scripts/InputHandler.cs
+++ Project/Assets/Scripts/InputHandler.cs
@@ -6,9 +6,24 @@ public class InputHandler : MonoBehaviour
     void start() { print("entro"); }
     void Update()
     {
+<<<<<<< Updated upstream
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, 100))
             print("yolo");
+=======
+         for (var i = 0; i < Input.touchCount; ++i)
+            {
+              if (Input.GetTouch(i).phase == TouchPhase.Began)
+              {            
+                    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                    if (Physics.Raycast(ray))
+                    {
+                        transform.Rotate(Vector3.up /8, Space.World);
+                    }
+   
+              }
+         }
+>>>>>>> Stashed changes
     }
 }
--- Project/Assets/Scripts/rotateAroundObject.cs
+++ Project/Assets/Scripts/rotateAroundObject.cs
@@ -7,13 +7,10 @@ public class rotateAroundObject : MonoBehaviour
     [SerializeField]
     private GameObject[] objects;
     private float rotation;
+    [SerializeField]
+    private float width;
+    [SerializeField]
     private float speed;
-
-    // Use this for initialization
-    void Start()
-    {
-        speed = 0.005f;
-    }
 
     // Update is called once per frame
     void Update()
@@ -21,12 +18,23 @@ public class rotateAroundObject : MonoBehaviour
         rotation += speed;
         for(int i = 0; i < objects.Length; i++)
         {
-            objects[i].transform.position = new Vector3(Mathf.Cos(rotation + 2* Mathf.PI / objects.Length * i)*10, 0, Mathf.Sin(rotation + 2 * Mathf.PI / objects.Length * i) * 10);
+            objects[i].transform.position = new Vector3(Mathf.Cos(rotation + 2* Mathf.PI / objects.Length * i)*width, 0, Mathf.Sin(rotation + 2 * Mathf.PI / objects.Length * i) * width);
+        }
+
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Rotate object using X plane
+            rotation += touchDeltaPosition.x/100;
         }
     }
-
+    /*
     private void OnMouseDrag()
     {
         rotation += Input.GetAxis("Mouse X")/20;
     }
+    */
 }
