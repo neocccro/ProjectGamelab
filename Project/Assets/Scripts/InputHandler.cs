4c9776fdc6f146f40f5fce58d2d57113b96ef5c8
Unknown <cavandiepen@gmail.com>
2018-05-25 10:25:26 +0200

    k

--- Project/Assets/Scripts/InputHandler.cs
+++ Project/Assets/Scripts/InputHandler.cs
@@ -6,17 +6,14 @@ public class InputHandler : MonoBehaviour
     void start() { print("entro"); }
     void Update()
     {
-<<<<<<< Updated upstream
-
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, 100))
             print("yolo");
-=======
          for (var i = 0; i < Input.touchCount; ++i)
             {
               if (Input.GetTouch(i).phase == TouchPhase.Began)
               {            
-                    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+                    ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
                     if (Physics.Raycast(ray))
                     {
                         transform.Rotate(Vector3.up /8, Space.World);
@@ -24,6 +21,5 @@ public class InputHandler : MonoBehaviour
    
               }
          }
->>>>>>> Stashed changes
     }
 }
